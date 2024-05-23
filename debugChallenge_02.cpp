37c9664ff54f93d1860fe0d8975f6c27d857a072
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-05-23 23:56:12 +0000

    correção

---
+++ debugChallenge_02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int num = 10;
+    
+    if (num = 10) {std::cout << "O número é igual a 10." << std::endl;
+    } else {
+        cout << "O número não é igual a 10." << std::endl;
+        cout << "Este é um código com erros" << std::endl;
+        cout << "Revise o código e aponte os erros" << std::endl;
+    }
+    return 0;
+}
